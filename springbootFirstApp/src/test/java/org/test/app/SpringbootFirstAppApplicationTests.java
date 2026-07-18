a2ef42b6301863ec39d0c5395e52ea5842c78885
chittaranjan <chittaranjan@hclguvi.com>
2026-07-18 13:03:43 +0530

    18-7-26

---
+++ springbootFirstApp/src/test/java/org/test/app/SpringbootFirstAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.test.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootFirstAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
