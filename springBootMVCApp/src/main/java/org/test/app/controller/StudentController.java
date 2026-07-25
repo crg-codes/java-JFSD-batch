ef3255a52a8910e80ce3eaa15c2e3eed024c7105
chittaranjan <chittaranjan@hclguvi.com>
2026-07-25 11:46:31 +0530

    25-7-26

--- springBootMVCApp/src/main/java/org/test/app/controller/StudentController.java
+++ springBootMVCApp/src/main/java/org/test/app/controller/StudentController.java
@@ -1,10 +1,15 @@
 package org.test.app.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.test.app.model.Student;
@@ -36,11 +41,79 @@ public class StudentController {
 		
 	}
 	
+	
+	
 	@GetMapping("list")
 	public String students(Model model)
 	{
 		model.addAttribute("students",this.studentRepo.findAll());
 		return "studentList";
+		
+	}
+	
+
+	@GetMapping("{sid}")
+	public String student(@PathVariable("sid")int sid,Model model)
+	{
+		//System.out.println(sid);
+		Optional<Student> student=this.studentRepo.findById(sid);
+		if(student.isPresent())
+		{
+		 model.addAttribute("student",student.get());
+		}
+		return "student";
+		
+	}
+	
+	@GetMapping("/delete/{sid}")
+	public String deleteStudent(@PathVariable("sid")int sid)
+	{
+		this.studentRepo.deleteById(sid);
+		return "redirect:/students/list";
+		
+	}
+	
+	@GetMapping("showFormForupdate/{sid}")
+	public String showFormForUpdate(@PathVariable("sid")int sid,Model model)
+	{
+		Optional<Student> student=this.studentRepo.findById(sid);
+		if(student.isPresent())
+		{
+		 model.addAttribute("student",student.get());
+		}
+		
+		return "update_form";
+	}
+	
+	@PostMapping("update/{sid}")
+	public String updateStudent(@PathVariable("sid")int sid,@ModelAttribute("student") Student student,BindingResult bindingResult,Model model)
+	{
+		
+		if(bindingResult.hasErrors())
+		{
+			return "update_form";
+		}
+		
+		Optional<Student> existingStudent=this.studentRepo.findById(sid);
+		if(existingStudent.isPresent())
+		{
+		 
+		Student oldStudentRecord=existingStudent.get();
+		System.out.println(oldStudentRecord);
+
+		 oldStudentRecord.setSname(student.getSname());
+		 oldStudentRecord.setCity(student.getCity());
+		 oldStudentRecord.setEmail(student.getEmail());
+		 this.studentRepo.save(oldStudentRecord);
+			
+		 return "redirect:/students/list";
+		}
+		else
+		{
+			return "redirect:/students/list";
+		}
+		
+		
 		
 	}
 }
